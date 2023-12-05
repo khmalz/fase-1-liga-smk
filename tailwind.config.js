b05c4dfab51649dfc0eb2dfccee42aacd08bcb33
khmalz <akmalrma1710@gmail.com>
2023-12-05 21:56:08 +0700

    move tailwind from cdn to local

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+   content: ["index.html"],
+   theme: {
+      extend: {},
+   },
+   plugins: [require("@tailwindcss/typography"), require("@tailwindcss/aspect-ratio"), require("@tailwindcss/forms")],
+};
